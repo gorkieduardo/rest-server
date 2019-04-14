d1fb435cb79075f468caf254e4e79c34533dedd4
Eduardo Estrada <ceed.educacion@gmail.com>
2019-04-14 20:00:22 -0300

    RestSever - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+//  if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/modelodb';
+//  } else {
+    urlDB = 'mongodb+srv://gorkiedu:rcNQruUVdqZKsFlv@cluster0-d2pir.mongodb.net/modelodb';
+    
+//}
+process.env.URLDB = urlDB;
+
