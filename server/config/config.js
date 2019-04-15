e6c7deea6d0db32cdeb005a3b235d13b160c7d31
Eduardo Estrada <ceed.educacion@gmail.com>
2019-04-14 21:30:05 -0300

    Mongo varia_entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-   urlDB = 'mongodb://localhost:27017/modelodb';
- } else {
+// if (process.env.NODE_ENV === 'dev') {
+//    urlDB = 'mongodb://localhost:27017/modelodb';
+//  } else {
     urlDB = process.env.MONGO_URI;
-}
+//}
 process.env.URLDB = urlDB;
 
